593240b7702159a82480eabd658a834743b6f2c3
Sumit Sahu <sumitsahu587@yahoo.in>
2020-04-23 01:15:45 +0530

    add services to share with components and in service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  // getHeroes(): Hero[] {
+  //   return HEROES;
+  // }
+  getHeroes(): Observable<Hero[]> {
+
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
